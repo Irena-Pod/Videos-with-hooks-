3b4a89a5a583448c4d3ab268a5ced3e2f706741c
Irena-Pod <irena.podgurski@gmail.com>
2021-03-19 19:32:36 -0400

    Add VideoDetail component

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,11 +1,11 @@
 import './VideoItem.css';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="video-item item">
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img
         className="ui image"
-        alt={video.snippet.description}
+        alt={video.snippet.title}
         src={video.snippet.thumbnails.medium.url}
       />
       <div className="content">
